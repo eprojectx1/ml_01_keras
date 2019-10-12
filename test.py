a4c314898a80f0a3bab01616a36719a5cae7cbc1
bosua3393 <bosua3393@gmail.com>
2019-10-12 18:58:07 -0400

    add run time

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import tensorflow as tf
+import time
+start = time.time()
 print(tf.__version__)
 mnist = tf.keras.datasets.mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 training_images = training_images/255
 test_images = test_images/255
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+with tf.device('/cpu:0'):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-model.fit(training_images, training_labels, epochs = 5)
-model.evaluate(test_images, test_labels)
-classifications = model.predict(test_images)
-print(classifications[0])
-print(test_labels[0])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+    model.fit(training_images, training_labels, epochs=5)
+    model.evaluate(test_images, test_labels)
+    classifications = model.predict(test_images)
+    print(classifications[0])
+    print(test_labels[0])
+
+    print(time.time() - start)
